d8d19fa23926de7be1747fa89687851ae0529659
Nikola <milje18@gmail.com>
2020-09-19 13:43:40 +0200

    fethcing github users with axios and passing props to users component, users, userItem and navbar component turned into functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class UserItem extends Component {
+const UserItem = ({user:{avatar_url,login,html_url}}) => {
 
-       state = {
-            id: 'id',
-            login: 'octocat',
-            avatar_url: 'https://github.com/images/error/octocat_happy.gif',
-            html_url: 'https://github.com/octocat'
-        }
-    render() {
-
-        const  {avatar_url,login,html_url} = this.state;
-
-        return (
-            <div className="card text-center">
-                <img src={avatar_url}  className="round-img" style={{width: '60px'}}/>
-                 <h3>{login}</h3>
-                 <div>
-                     <a href={html_url} className="btn btn-dark btn-sm my-1">more</a>
-                 </div>
+    return (
+        <div className="card text-center">
+            <img src={avatar_url}  className="round-img" style={{width: '60px'}}/>
+            <h3>{login}</h3>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">more</a>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default UserItem
